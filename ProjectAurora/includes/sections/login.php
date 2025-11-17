df469520ada9c5988cbfffe730b4fc0005716367
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-11-16 23:00:42 -0600

    Delete ProjectAurora directory

--- ProjectAurora/includes/sections/login.php
+++
@@ -1,4 +0,0 @@
-<div class="section-content overflow-y active" data-section="login">
-    <h1>Página de Login</h1>
-    <p>Contenido de la sección de login cargado dinámicamente.</p>
-    </div>
--- ProjectAurora/includes/sections/main.php
+++
@@ -1,4 +0,0 @@
-<div class="section-content overflow-y active" data-section="main">
-    <h1>Página Principal (Main)</h1>
-    <p>Contenido de la sección principal cargado dinámicamente.</p>
-</div>
