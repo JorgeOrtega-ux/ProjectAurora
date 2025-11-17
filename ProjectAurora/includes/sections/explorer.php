562f9b199f06c9002fb03347c8f031b5e6d8ecdd
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-11-17 12:09:58 -0600

    Delete ProjectAurora directory

--- ProjectAurora/includes/sections/explorer.php
+++
@@ -1,4 +0,0 @@
-<div class="section-content overflow-y active" data-section="explorer">
-    <h1>Explorar Comunidades</h1>
-    <p>Descubre nuevas comunidades y únete a la conversación.</p>
-    </div>
