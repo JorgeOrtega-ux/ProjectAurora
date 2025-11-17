0be33fce1ec0ac6fb3528a192073e432bff4db0e
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-11-17 12:56:27 -0600

    Add files via upload

--- ProjectAurora/includes/sections/404.php
+++ ProjectAurora/includes/sections/404.php
@@ -1,6 +1,5 @@
 <?php
 // Esto asegura que el $basePath (definido en router.php) esté disponible
-// para el enlace de "Go back home".
 if (!isset($basePath)) {
     $basePath = '/ProjectAurora/';
 }
@@ -9,13 +8,12 @@ if (!isset($basePath)) {
 
     <div class="section-center-wrapper">
 
-        <div class="error-404-container"> <span class="error-404-badge">404</span> <h1 class="error-404-title">Oops! Page not found.</h1> <p class="error-404-text"> We couldn't find the page you're looking for. It might have been moved or doesn't exist anymore.
+        <div class="error-404-container"> 
+            <span class="error-404-badge">404</span> 
+            <h1 class="error-404-title">¡Ups! Página no encontrada.</h1> 
+            <p class="error-404-text"> 
+                No pudimos encontrar la página que buscas. Es posible que se haya movido o ya no exista.
             </p>
-
-            <a href="<?php echo htmlspecialchars($basePath, ENT_QUOTES, 'UTF-8'); ?>" class="form-button" style="max-width: 220px; margin: 24px auto 0; text-decoration: none; display: block; text-align: center; line-height: 55px;">
-                Go back home
-            </a>
-
         </div>
 
     </div>
