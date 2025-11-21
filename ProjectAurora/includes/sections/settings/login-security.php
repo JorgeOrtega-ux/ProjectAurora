cf388187666c6ef0cda92e61ea525df306a6e19d
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-11-20 20:52:25 -0600

    Delete ProjectAurora directory

--- ProjectAurora/includes/sections/settings/login-security.php
+++
@@ -1,4 +0,0 @@
-<div class="section-content overflow-y active" data-section="settings/login-security">
-    <h1>Inicio de sesión y seguridad</h1>
-    <p>Gestiona tu contraseña y autenticación en dos pasos.</p>
-</div>
