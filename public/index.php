8416fc2642f9cf80c3f0dbbcd96a0b9ad74628b5
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-12-09 12:22:28 -0600

    .

---
+++ public/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <title>ProjectAurora</title>
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <div class="main-content">
+            <div class="general-content">
+                <div class="general-content-top">
+                    <div class="header">
+                        <div class="header-left">
+                            <div class="header-button">
+                                <span class="material-symbols-rounded">menu</span>
+                            </div>
+                        </div>
+                        <div class="header-center">
+                            <div class="search-wrapper">
+                                <div class="search-container">
+                                    <div class="search-icon">
+                                        <span class="material-symbols-rounded">search</span>
+                                    </div>
+                                    <div class="search-input">
+                                        <input type="text">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-right">
+                            <div class="header-item"> 
+                                <div class="header-button">
+                                    <span class="material-symbols-rounded">more_vert</span>
+                                </div>
+                               
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="general-content-bottom">
+                    <div class="general-content-scrolleable"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
