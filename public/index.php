a6a32334dc56592f6ced12e5018d7352021f6053
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-12-09 13:05:05 -0600

    .

--- public/index.php
+++ public/index.php
@@ -16,39 +16,109 @@
                 <div class="general-content-top">
                     <div class="header">
                         <div class="header-left">
-                            <div class="header-button">
+                            <div class="header-button" data-action="toggleModuleSurface">
                                 <span class="material-symbols-rounded">menu</span>
                             </div>
                         </div>
-                        <div class="header-center">
+
+                        <div class="header-center" id="headerCenter">
                             <div class="search-wrapper">
                                 <div class="search-container">
                                     <div class="search-icon">
                                         <span class="material-symbols-rounded">search</span>
                                     </div>
                                     <div class="search-input">
-                                        <input type="text">
+                                        <input type="text" placeholder="Buscar...">
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                         <div class="header-right">
-                            <div class="header-item"> 
-                                <div class="header-button">
+                            <div class="header-item">
+                                <div class="header-button search-toggle-btn" id="searchToggleBtn">
+                                    <span class="material-symbols-rounded">search</span>
+                                </div>
+
+                                <div class="header-button" data-action="toggleModuleProfile">
                                     <span class="material-symbols-rounded">more_vert</span>
                                 </div>
-                               
                             </div>
-
+                            <div class="module-content module-profile disabled" data-module="moduleProfile">
+                                <div class="menu-content">
+                                    <div class="menu-list">
+                                        <div class="menu-link">
+                                            <div class="menu-link-icon">
+                                                <span class="material-symbols-rounded">settings</span>
+                                            </div>
+                                            <div class="menu-link-text">
+                                                <span>Configuracion</span>
+                                            </div>
+                                        </div>
+                                        <div class="menu-link">
+                                            <div class="menu-link-icon">
+                                                <span class="material-symbols-rounded">close</span>
+                                            </div>
+                                            <div class="menu-link-text">
+                                                <span>Cerrar sesion</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
                 <div class="general-content-bottom">
+                    <div class="module-content module-surface disabled" data-module="moduleSurface">
+                        <div class="menu-content">
+                            <div class="menu-content-top">
+                                <div class="menu-list">
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">home</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Pagina principal</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">home</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Explorar colecciones</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="menu-content-bottom"></div>
+                        </div>
+                    </div>
                     <div class="general-content-scrolleable"></div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const searchBtn = document.getElementById('searchToggleBtn');
+            const headerCenter = document.getElementById('headerCenter');
+            const searchInput = headerCenter.querySelector('input');
+
+            searchBtn.addEventListener('click', () => {
+                // Alternar la clase 'active'
+                headerCenter.classList.toggle('active');
+
+                // Opcional: enfocar el input si se abre
+                if (headerCenter.classList.contains('active')) {
+                    searchInput.focus();
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
