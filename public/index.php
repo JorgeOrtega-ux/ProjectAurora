013ca850efd545c558c92b138335a4b6257f6ae0
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2026-02-17 12:42:25 -0600

    --

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
     <link rel="stylesheet" type="text/css" href="assets/css/components/components.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" />
-    <title>Document</title>
+    <title>Project Aurora</title>
 </head>
 
 <body>
@@ -18,10 +18,8 @@
                     <div class="header">
                         <div class="header-left">
                             <div class="component-actions">
-                                <button class="component-button component-button--square-40">
-                                    <span class="material-symbols-rounded">
-                                        menu
-                                    </span>
+                                <button class="component-button component-button--square-40" data-action="toggleModuleSurface">
+                                    <span class="material-symbols-rounded">menu</span>
                                 </button>
                             </div>
                         </div>
@@ -43,44 +41,69 @@
                                     <span class="material-symbols-rounded">search</span>
                                 </button>
 
-                                <button class="component-button component-button--square-40">
-                                    <span class="material-symbols-rounded">
-                                        more_vert
-                                    </span>
+                                <button class="component-button component-button--square-40" data-action="toggleModuleMainOptions">
+                                    <span class="material-symbols-rounded">more_vert</span>
                                 </button>
                             </div>
                         </div>
 
-                        <div class="module-content">
-                            <div class="menu-content">
-                                <div class="menu-list">
-                                    <div class="menu-link">
-                                        <div class="menu-link-icon">
-                                            <span class="material-symbols-rounded">
-                                                settings
-                                            </span>
+                        <div class="component-module component-module--display-overlay component-module--size-m disabled" data-module="moduleMainOptions">
+                            <div class="component-module-panel component-module-panel--padded">
+                                <div class="component-menu-list">
+                                    <a href="#" class="component-menu-link">
+                                        <div class="component-menu-link-icon">
+                                            <span class="material-symbols-rounded">settings</span>
                                         </div>
-                                        <div class="menu-link-text">
+                                        <div class="component-menu-link-text">
                                             <span>Configuracion</span>
                                         </div>
-                                    </div>
-                                    <div class="menu-link">
-                                        <div class="menu-link-icon">
-                                          <span class="material-symbols-rounded">
-help
-</span>
+                                    </a>
+                                    <a href="#" class="component-menu-link">
+                                        <div class="component-menu-link-icon">
+                                            <span class="material-symbols-rounded">help</span>
                                         </div>
-                                        <div class="menu-link-text">
+                                        <div class="component-menu-link-text">
                                             <span>Ayuda y comentarios</span>
                                         </div>
-                                    </div>
+                                    </a>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
+
                 <div class="general-content-bottom">
-                    <div class="general-content-scrolleable"></div>
+                    
+                    <div class="component-module component-module--display-block component-module--size-m component-module--offset-s disabled" data-module="moduleSurface">
+                        <div class="component-module-panel">
+                            <div class="component-module-panel-top">
+                                <div class="component-menu-list">
+                                    <a href="#" class="component-menu-link">
+                                        <div class="component-menu-link-icon">
+                                            <span class="material-symbols-rounded">home</span>
+                                        </div>
+                                        <div class="component-menu-link-text">
+                                            <span>Página principal</span>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="component-menu-link">
+                                        <div class="component-menu-link-icon">
+                                            <span class="material-symbols-rounded">trending_up</span>
+                                        </div>
+                                        <div class="component-menu-link-text">
+                                            <span>Explorar tendencias</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="component-module-panel-bottom">
+                                </div>
+                        </div>
+                    </div>
+
+                    <div class="general-content-scrolleable">
+                        </div>
                 </div>
             </div>
         </div>
