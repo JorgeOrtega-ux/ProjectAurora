7b91abbdde8c6f41ed37e447a63269f5612a11af
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2026-02-19 14:31:05 -0600

    --

--- includes/modules/moduleMainOptions.php
+++ includes/modules/moduleMainOptions.php
@@ -6,43 +6,48 @@ $isLoggedInOption = isset($_SESSION['user_id']);
 $settingsRoute = $isLoggedInOption ? '/ProjectAurora/settings/your-account' : '/ProjectAurora/settings/guest';
 ?>
 <div class="component-module component-module--display-overlay component-module--size-m disabled" data-module="moduleMainOptions">
-    <div class="component-module-panel component-module-panel--padded">
+    <div class="component-module-panel">
+        
         <div class="pill-container">
             <div class="drag-handle"></div>
         </div>
-        <div class="component-menu-list">
-            
-            <a href="<?php echo $settingsRoute; ?>" class="component-menu-link nav-item" data-nav="<?php echo $settingsRoute; ?>">
-                <div class="component-menu-link-icon">
-                    <span class="material-symbols-rounded">settings</span>
-                </div>
-                <div class="component-menu-link-text">
-                    <span>Configuracion</span>
-                </div>
-            </a>
-            
-            <a href="#" class="component-menu-link">
-                <div class="component-menu-link-icon">
-                    <span class="material-symbols-rounded">help</span>
-                </div>
-                <div class="component-menu-link-text">
-                    <span>Ayuda y comentarios</span>
-                </div>
-            </a>
-            
-            <?php if ($isLoggedInOption): ?>
-                <div style="height: 1px; background-color: #eee; margin: 4px 0;"></div>
 
-                <a href="#" class="component-menu-link" data-action="logout">
+        <div class="component-module-panel-body component-module-panel-body--padded">
+            <div class="component-menu-list">
+                
+                <a href="<?php echo $settingsRoute; ?>" class="component-menu-link nav-item" data-nav="<?php echo $settingsRoute; ?>">
                     <div class="component-menu-link-icon">
-                        <span class="material-symbols-rounded">logout</span>
+                        <span class="material-symbols-rounded">settings</span>
                     </div>
                     <div class="component-menu-link-text">
-                        <span>Cerrar sesión</span>
+                        <span>Configuración</span>
                     </div>
                 </a>
-            <?php endif; ?>
+                
+                <a href="#" class="component-menu-link">
+                    <div class="component-menu-link-icon">
+                        <span class="material-symbols-rounded">help</span>
+                    </div>
+                    <div class="component-menu-link-text">
+                        <span>Ayuda y comentarios</span>
+                    </div>
+                </a>
+                
+                <?php if ($isLoggedInOption): ?>
+                    <div style="height: 1px; background-color: #eee; margin: 4px 0; flex-shrink: 0;"></div>
 
+                    <a href="#" class="component-menu-link" data-action="logout">
+                        <div class="component-menu-link-icon">
+                            <span class="material-symbols-rounded">logout</span>
+                        </div>
+                        <div class="component-menu-link-text">
+                            <span>Cerrar sesión</span>
+                        </div>
+                    </a>
+                <?php endif; ?>
+
+            </div>
         </div>
+
     </div>
 </div>
